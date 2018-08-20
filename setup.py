a682b00e850ed190549d446392858125a49e63f0
Ron Thompson <re.thompsoniii@gmail.com>
2018-08-20 11:00:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,4 @@ setup(name='koalas',
       version='0.0.0',
       description='Pandas Like Wrapper for Spark',
       author='Ron Thompson',
-      author_email='ron.thompson@point72.com',
       packages=['koalas'])
